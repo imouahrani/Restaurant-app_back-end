9fd91892bcb1bb24294717e90c600e1d4a89fa6a
Imene <imene.ouahrani@capgemini.com>
2023-04-12 13:53:42 +0200

    RESTO-O1 : models - Restaurant.java

---
+++ src/main/java/com/formation/restaurant/models/Restaurant.java
@@ -0,0 +1,31 @@
+package com.formation.restaurant.models;
+
+public class Restaurant {
+    private String id;
+    private String nom;
+    private String adresse;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+}
